eb98c151957ffde18e5a2fea0fb76d02e7abee86
IrinKovalyova <93247756+IrinKovalyova@users.noreply.github.com>
2021-10-27 10:28:41 +0300

    Add files via upload

---
+++ task 4.py
@@ -0,0 +1,10 @@
+a = int(input())
+if (a>=1 and a<3):
+    print("зима")
+if (a>=3 and a<6):
+    print("весна")
+if (a>5 and a<9):
+    print("лето")
+if (a>8 and a<12):
+    print("осень")
+
---
+++ task1.py
@@ -0,0 +1,11 @@
+m = int(input())
+n = int(input())
+p = int(input())
+count = 0
+if m < 0:
+    count += 1
+if n < 0:
+    count += 1
+if p < 0:
+    count += 1
+print(count)
