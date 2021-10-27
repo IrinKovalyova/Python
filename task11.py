eda9347e691355b6f37260b6bdc091c432f333fa
IrinKovalyova <93247756+IrinKovalyova@users.noreply.github.com>
2021-10-28 00:48:03 +0300

    Add files via upload

---
+++ task11.py
@@ -0,0 +1,25 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = 0
+max = 0
+
+if max > a:
+    max = max
+else:
+    max = a
+
+if max > b:
+    max = max
+else:
+    max = b
+
+if max > c:
+    max = max
+else:
+    max = c
+d = max
+print(d)
+
+
+
