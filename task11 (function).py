f985e55e13c1c4fa6311c04f59b23dd8e329c5f2
IrinKovalyova <93247756+IrinKovalyova@users.noreply.github.com>
2021-10-31 22:17:26 +0300

    Add files via upload

---
+++ task11 (function).py
@@ -0,0 +1,67 @@
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# function with args
+
+def max_from_three_numbers(a,b,c):
+    d = 0
+    max = 0
+
+    if max > a:
+        max = max
+    else:
+        max = a
+
+    if max > b:
+        max = max
+    else:
+        max = b
+
+    if max > c:
+        max = max
+    else:
+        max = c
+    d = max
+    return d
+
+f = max_from_three_numbers(1,2,3)
+print(f)
+
+# function without args
+
+def max_from_three_numbers():
+    d = 0
+    max = 0
+
+    a = 1
+    b = 2
+    c = 3
+    if max > a:
+        max = max
+    else:
+        max = a
+
+    if max > b:
+        max = max
+    else:
+        max = b
+
+    if max > c:
+        max = max
+    else:
+        max = c
+    d = max
+    return d
+
+g = max_from_three_numbers()
+print(g)
+
+
+
+
+
+
+
+
+
